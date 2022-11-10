b17aa3f99aea38aa78146e62bb879191d9ebabf0
AlexandrSV0 <semin.alex222@yandex.ru>
2022-11-10 21:08:54 +0300

    create

---
+++ lab1/main.py
@@ -0,0 +1,20 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
+
+num_a = int(input("Введите константу A: "))
+num_B = int(input("Введите константу B: "))
+num = int(input("Введите количество точек графика: "))
+
+plt.title('$y^2 = x^3 / (x - a)$')
+plt.xlabel('x')
+plt.ylabel('y')
+
+x = np.linspace(0, num_B, num,True)
+y = pow(pow(x,3) / (x - num_a), 0.5)
+y1 = -pow(pow(x,3)/ (x -num_a), 0.5)
+
+plt.grid()
+plt.plot(x, y, '-b', x, y1, '-b')
+plt.show()
